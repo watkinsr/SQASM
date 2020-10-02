a9aef1754a7bbfc32c6c10ab0d39ff10e1da0b60
watkinsr <ryanwatkins54@gmail.com>
2020-10-02 07:05:12 +0100

    Refactor project
    - Begin progress on interpreter (rebuild in C)
    - Remove yacc + flex (too much complexity)
    - We will port from python to C, in theory this should be a trivial matter
    - Right now the interpreter handles one command INIT

---
+++ include/parse.h
@@ -0,0 +1,18 @@
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+void lsh_loop(void);
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+int lsh_exit(char **args);
+
+int lsh_init(char **args);
+
+char *tokens[] = {
+    "INIT"};
+
+int (*builtin_func[])(char **) = {
+    &lsh_init};
+// &lsh_u,
+// &lsh_apply};
---
+++ src/parse.c
@@ -0,0 +1,157 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../include/parse.h"
+
+// Taken from: https://brennan.io/2015/01/16/write-a-shell-in-c/
+
+int main(int argc, char **argv)
+{
+    // Load config files, if any.
+
+    // Run command loop.
+    lsh_loop();
+
+    // Perform any shutdown/cleanup.
+    return 0;
+}
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+char *lsh_read_line(void)
+{
+    char *line = NULL;
+    ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            exit(EXIT_SUCCESS); // We recieved an EOF
+        }
+        else
+        {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+char **lsh_split_line(char *line)
+{
+    int bufsize = LSH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+
+    while (token != NULL)
+    {
+        printf("token: %s\n", token);
+        tokens[position++] = token;
+
+        if (position >= bufsize)
+        {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+
+int lsh_num_builtins()
+{
+    return sizeof(tokens) / sizeof(char *);
+}
+
+int lsh_init(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "we require a register to INIT, example: INIT R2 2 0\n");
+    }
+    else
+    {
+        printf("Received a register\n");
+    }
+    return 1;
+}
+
+/*
+  Builtin function implementations.
+*/
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Welcome to this simple quantum interpreter (SQINT)");
+    printf("Consult examples for example program text");
+
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
+
+int lsh_execute(char **args)
+{
+    int i;
+
+    if (args[0] == NULL)
+    {
+        // An empty command was entered.
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        if (strcmp(args[0], tokens[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    // return lsh_launch(args);
+}
