4937fb3fa96152c2af547dbaf677243036801093
watkinsr <ryanwatkins54@gmail.com>
2021-08-29 02:18:25 +0200

    Various QOL improvements
    Now we can see help in the shell and peek at registers. There is also
    basic variable allocation for gates and registers.

--- include/parse.h
+++ include/parse.h
@@ -1,23 +1,35 @@
 #include <string>
+#include <unordered_map>
+#include <string>
+#include "QReg.h"
 
 #define LSH_RL_BUFSIZE 1024
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 
-void lsh_loop(void);
-char *lsh_read_line(void);
-char **lsh_split_line(char *line);
-int lsh_exit(char **args);
-int lsh_execute(char **args);
-int lsh_init(char **args);
-int lsh_formgate(char **args);
+void interpreter_loop(void);
+char *read_line(void);
+char **split_line(char *line);
+int exit(char **args);
+int execute_cmd(char **args);
+int sqint_init(char **args);
+int sqint_formgate(char **args);
+int sqint_peek(char **args);
+int shell_help(char **args);
+
+unordered_map<string, QuantumGate> gateHashmap;
+unordered_map<string, QReg> quantumRegisterHashmap;
 
 const char *tokens[] = {
     "INIT",
-    "FORMGATE"
+    "FORMGATE",
+    "PEEK",
+    "help",
 };
 
 int (*builtin_func[])(char **) = {
-    &lsh_init,
-    &lsh_formgate
+    &sqint_init,
+    &sqint_formgate,
+    &sqint_peek,
+    &shell_help,
 };
--- src/parse.cpp
+++ src/parse.cpp
@@ -1,178 +1,166 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-#include "../include/parse.h"
 #include "../include/QReg.h"
+#include "../include/parse.h"
 
-// Taken from: https://brennan.io/2015/01/16/write-a-shell-in-c/
-int main(void)
-{
-    // Run command loop.
-    lsh_loop();
-
-    return 0;
+int main(void) {
+  interpreter_loop();
+  return 0;
 }
 
-void lsh_loop(void)
-{
-    char *line;
-    char **args;
-    int status;
+void interpreter_loop(void) {
+  char *line;
+  char **args;
+  int status;
 
-    do
-    {
-        printf("> ");
-        line = lsh_read_line();
-        args = lsh_split_line(line);
-        status = lsh_execute(args);
+  do {
+    printf("> ");
+    line = read_line();
+    args = split_line(line);
+    status = execute_cmd(args);
 
-        free(line);
-        free(args);
-    } while (status);
+    free(line);
+    free(args);
+  } while (status);
 }
 
-char *lsh_read_line(void)
-{
-    char *line = NULL;
-    size_t bufsize = 0; // have getline allocate a buffer for us
+char *read_line(void) {
+  char *line = NULL;
+  size_t bufsize = 0; // have getline allocate a buffer for us
 
-    if (getline(&line, &bufsize, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
-            exit(EXIT_SUCCESS); // We recieved an EOF
-        }
-        else
-        {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
+  if (getline(&line, &bufsize, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS); // We recieved an EOF
+    } else {
+      perror("readline");
+      exit(EXIT_FAILURE);
     }
+  }
 
-    return line;
+  return line;
 }
 
-char **lsh_split_line(char *line)
-{
-    size_t bufsize = LSH_TOK_BUFSIZE;
-    size_t position = 0;
-    char **tokens = (char **)malloc(bufsize * sizeof(char *));
-    char *token;
+char **split_line(char *line) {
+  size_t bufsize = LSH_TOK_BUFSIZE;
+  size_t position = 0;
+  char **tokens = (char **)malloc(bufsize * sizeof(char *));
+  char *token;
 
-    if (!tokens)
-    {
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+
+  while (token != NULL) {
+    // printf("token: %s\n", token);
+    tokens[position++] = token;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = (char **)realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens) {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
+      }
     }
 
-    token = strtok(line, LSH_TOK_DELIM);
-
-    while (token != NULL)
-    {
-        // printf("token: %s\n", token);
-        tokens[position++] = token;
-
-        if (position >= bufsize)
-        {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = (char **)realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, LSH_TOK_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
 
 /*
   List of builtin commands, followed by their corresponding functions.
  */
 
-int lsh_num_builtins()
-{
-    return sizeof(tokens) / sizeof(char *);
+int get_num_commands() { return sizeof(tokens) / sizeof(char *); }
+
+int sqint_init(char **args) {
+  if (args[1] == NULL || args[2] == NULL || args[3] == NULL) {
+    fprintf(stderr, "invalid args to INIT, example: INIT R2 2 0\n");
+  } else {
+    int amountOfQubits = atoi(args[2]);
+    QReg reg = QReg(amountOfQubits, atoi(args[3]));
+    // First check if it exists
+    auto iter = quantumRegisterHashmap.find(args[1]);
+    if (iter != quantumRegisterHashmap.end()) {
+      quantumRegisterHashmap.erase(args[1]);
+    }
+
+    auto registerVariablePair = std::pair<string, QReg>(args[1], reg);
+    quantumRegisterHashmap.insert(registerVariablePair);
+    reg.printAmplitudes();
+  }
+  return 1;
 }
 
-int lsh_init(char **args)
-{
-    if (args[1] == NULL || args[2] == NULL || args[3] == NULL)
-    {
-        fprintf(stderr, "invalid args to INIT, example: INIT R2 2 0\n");
-    }
-    else
-    {
-        int amountOfQubits = atoi(args[2]);
-        QReg reg = QReg(amountOfQubits, 2);
-        auto U = reg.tensor(reg.HAD_GATE, reg.ID_GATE);
-        reg.applyGateToSystem(U);
-        reg.applyGateToSystem(reg.CNOT_GATE);
-        reg.printAmplitudes();
+int sqint_peek(char **args) {
+  if (args[1] == NULL) {
+    fprintf(stderr, "bad input");
+    return 1;
+  } else {
+    auto iter = quantumRegisterHashmap.find(args[1]);
+    if (iter == quantumRegisterHashmap.end()) {
+      printf("Couldn't find variable.\n");
+    } else {
+      iter->second.printAmplitudes();
     }
     return 1;
+  }
 }
 
-int lsh_formgate(char **args)
-{
-    if (args[1] == NULL || args[2] == NULL || args[3] == NULL)
-    {
-        fprintf(stderr, "bad input");
-        return 1;
-    }
-    else
-    {
-        try
-        {
-            auto gate1 = QReg::getGateByString(args[2]);
-            auto gate2 = QReg::getGateByString(args[3]);
-            auto U = QReg::tensor(gate1, gate2);
-            QReg::printGate(U);
-            return 1;
-        }
-        catch (const std::invalid_argument& e)
-        {
-            printf("Error: %s\n", e.what());
-            return 1;
-        }
-    }
-}
-
-
-int lsh_help(char **args)
-{
-    printf("SQINT - Simple Quantum Interpreter\n");
-    printf("To initialize a register use INIT (register, numberOfQubits, initBit)\n");
+int sqint_formgate(char **args) {
+  if (args[1] == NULL || args[2] == NULL || args[3] == NULL) {
+    fprintf(stderr, "bad input");
     return 1;
+  } else {
+    auto gate1 = QReg::getGateByString(args[2]);
+    auto gate2 = QReg::getGateByString(args[3]);
+    auto U = QReg::tensor(gate1, gate2);
+    auto gatePair = std::pair<string, QuantumGate>(args[1], U);
+    gateHashmap.insert(gatePair);
+    auto iter = gateHashmap.find(args[1]);
+    if (iter == gateHashmap.end()) {
+      printf("Couldn't find variable.\n");
+    } else {
+      QReg::printGate(iter->second);
+    }
+    return 1;
+  }
 }
 
-int lsh_exit(char **args)
-{
-    return 0;
+int shell_help(char **args) {
+  printf("SQINT - Simple Quantum Interpreter\n");
+  printf("To initialize a register use INIT REG QUBITSIZE INITBIT\n");
+  printf("To form a gate use FORMGATE VAR GATE1 GATE2\n");
+  printf("To peek at a register use PEEK REGISTERNAME\n");
+  return 1;
 }
 
-int lsh_execute(char **args)
-{
-    int i;
+int exit(char **args) { return 0; }
 
-    if (args[0] == NULL)
-    {
-        // An empty command was entered.
-        return 1;
-    }
+int execute_cmd(char **args) {
+  int i;
 
-    for (i = 0; i < lsh_num_builtins(); i++)
-    {
-        if (strcmp(args[0], tokens[i]) == 0)
-        {
-            return (*builtin_func[i])(args);
-        }
+  if (args[0] == NULL) {
+    return 1;
+  }
+
+  for (i = 0; i < get_num_commands(); i++) {
+    if (strcmp(args[0], tokens[i]) == 0) {
+      return (*builtin_func[i])(args);
     }
+  }
+  if (strcmp(args[0], "exit") == 0) {
     return 0;
+  } else {
+    return shell_help(args);
+  }
 }
